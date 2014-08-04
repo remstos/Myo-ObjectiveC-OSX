9c19ed78eb9d0100a7477431e153ac577815caa7
KemCake <santos.remi@icloud.com>
2014-08-05 01:54:05 +0200

    Initial Commit

---
+++ MyoObjectiveCOSX/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  MyoObjectiveCOSX
+//
+//  Created by Remi Santos on 05/08/2014.
+//  Copyright (c) 2014 Remi Santos. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ MyoObjectiveCOSX/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MyoObjectiveCOSX
+//
+//  Created by Remi Santos on 05/08/2014.
+//  Copyright (c) 2014 Remi Santos. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
