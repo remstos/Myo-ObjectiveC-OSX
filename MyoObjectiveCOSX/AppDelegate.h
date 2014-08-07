028cdd4623e1ae092f8d2a9647180a9177a2da4d
KemCake <santos.remi@icloud.com>
2014-08-07 15:46:43 +0200

    First Commit - Myo + MyoDelegate + MyoPost + MyoVector

--- MyoObjectiveCOSX/AppDelegate.h
+++ MyoObjectiveCOSX/AppDelegate.h
@@ -7,8 +7,8 @@
 //
 
 #import <Cocoa/Cocoa.h>
-
-@interface AppDelegate : NSObject <NSApplicationDelegate>
+#import "MyoObjectiveC.h"
+@interface AppDelegate : NSObject <NSApplicationDelegate, MyoDelegate>
 
 @property (assign) IBOutlet NSWindow *window;
 
