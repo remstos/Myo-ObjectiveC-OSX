7090da00f4fe2e039e225ed5c4dbeab94dbcc85c
ShirazO <smokemeout@gmail.com>
2014-09-08 03:51:24 +0530

    Update MyoObjectiveC.h
    added missing gestures

--- MyoObjectiveC.h
+++ MyoObjectiveC.h
@@ -12,50 +12,60 @@
 
 #pragma mark - MYOPOSE
 typedef enum MyoPoseType {
-    MyoPoseTypeFist = 0,
-    MyoPoseTypeFingersSpread = 1,
+    MyoPoseTypeRest = 0,
+    MyoPoseTypeFist = 1,
     MyoPoseTypeWaveIn = 2,
     MyoPoseTypeWaveOut = 3,
-    MyoPoseTypeThumbToPinky = 4
+    MyoPoseTypeFingersSpread = 4,
+    MyoPoseTypeReserved1 = 5,
+    MyoPoseTypePinkyToThumb = 6
 } MyoPoseType;
+
 @interface MyoPose : NSObject
-@property (nonatomic)MyoPoseType poseType;
+
+@property (nonatomic) MyoPoseType poseType;
+
 @end
 
-@interface MyoVector : NSObject
-{
+@interface MyoVector : NSObject {
+    
     float _data[3];
 }
+
 @property (nonatomic, readonly, getter=x)float x;
 @property (nonatomic, readonly, getter=y)float y;
 @property (nonatomic, readonly, getter=y)float z;
 @property (nonatomic, readonly, getter=magnitude)float magnitude;
 
--(id)init;
--(id)initWithX:(float)x y:(float)y z:(float)z;
--(float)x;
--(float)y;
--(float)z;
--(float)magnitude;
--(float)productWithVector:(MyoVector*)rhs;
--(MyoVector*)normalized;
--(MyoVector*)crossProductWithVector:(MyoVector*)rhs;
--(float)angleWithVector:(MyoVector *)rhs;
+- (id)init;
+- (id)initWithX:(float)x y:(float)y z:(float)z;
+- (float)x;
+- (float)y;
+- (float)z;
+- (float)magnitude;
+- (float)productWithVector:(MyoVector *)rhs;
+- (MyoVector *)normalized;
+- (MyoVector *)crossProductWithVector:(MyoVector *)rhs;
+- (float)angleWithVector:(MyoVector *)rhs;
+
 @end
+
 
 #pragma mark - MYODELEGATE
 @protocol MyoDelegate <NSObject>
+
 @optional
--(void)myoOnArmLost:(Myo*)myo;
--(void)myoOnArmRecognized:(Myo*)myo;
--(void)myoOnPair:(Myo*)myo;
--(void)myoOnConnect:(Myo*)myo;
--(void)myoOnDisconnect:(Myo*)myo;
--(void)myo:(Myo*)myo onPose:(MyoPose *)pose;
--(void)myo:(Myo*)myo onOrientationDataWithRoll:(int)roll pitch:(int)pitch yaw:(int)yaw;
--(void)myo:(Myo*)myo onAccelerometerDataWithVector:(MyoVector*)vector;
--(void)myo:(Myo*)myo onGyroscopeDataWithVector:(MyoVector*)vector;
--(void)myo:(Myo*)myo onRssi:(int8_t)rssi;
+- (void)myoOnPair:(Myo *)myo;
+- (void)myoOnConnect:(Myo *)myo;
+- (void)myoOnArmLost:(Myo *)myo;
+- (void)myoOnDisconnect:(Myo *)myo;
+- (void)myoOnArmRecognized:(Myo *)myo;
+- (void)myo:(Myo *)myo onRssi:(int8_t)rssi;
+- (void)myo:(Myo *)myo onPose:(MyoPose *)pose;
+- (void)myo:(Myo *)myo onGyroscopeDataWithVector:(MyoVector *)vector;
+- (void)myo:(Myo *)myo onAccelerometerDataWithVector:(MyoVector *)vector;
+- (void)myo:(Myo *)myo onOrientationDataWithRoll:(int)roll pitch:(int)pitch yaw:(int)yaw;
+
 @end
 
 
@@ -66,14 +76,18 @@ typedef enum MyoVibrationType {
     MyoVibrationTypeLong = 2
 } MyoVibrationType;
 
+
 #pragma mark - MYO
 @interface Myo : NSObject
-- (instancetype)initWithApplicationIdentifier:(NSString*)identifier;
--(BOOL)connectMyoWaiting:(int)milliseconds;
--(void)startUpdate;
--(void)stopUpdate;
--(void)vibrateWithType:(MyoVibrationType)type;
+
+- (instancetype)initWithApplicationIdentifier:(NSString *)identifier;
+- (BOOL)connectMyoWaiting:(int)milliseconds;
+- (void)stopUpdate;
+- (void)startUpdate;
+- (NSString *)poseName:(MyoPose *)pose;
+- (void)vibrateWithType:(MyoVibrationType)type;
 
 @property (nonatomic) int updateTime;
-@property (nonatomic, assign) id<MyoDelegate> delegate;
+@property (nonatomic, assign) id <MyoDelegate> delegate;
+
 @end
